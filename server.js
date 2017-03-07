6efdfdb1e80bb80c3badf78b70bc55388056df20
Samuel Y <samuel.y@weimeit.com>
2017-03-07 13:01:53 +0800

    .gitignore added

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const eo = require('express');
 app = eo();
 port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
 
-	expressLayout = require('express-ejs-layouts');
+mongoose = require('mongoose');
+mongoose.connect(process.env.db_URI);
 app.use(eo.static(__dirname+'/public/css'));
 	app.use(expressLayout);
 app.use(require('./app/router.js'));
